17ade9212141ff7733a142d230ea3e5eb443d8f4
JoshCyril <joshua.cyril88@gmail.com>
2023-12-12 03:21:04 +0000

    ✨Added: Filter

--- app/Livewire/QuoteList.php
+++ app/Livewire/QuoteList.php
@@ -4,15 +4,25 @@ namespace App\Livewire;
 
 use App\Models\Quote;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class QuoteList extends Component
 {
     use WithPagination;
+
+    #[Url()]
+    public $sort = 'desc';
+
+    public function setSort($sort) {
+        $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+        $this->resetPage();
+    }
+
     #[Computed()]
     public function quotes(){
-        return Quote::quoted()->orderBy('quoted_at', 'desc')->paginate(5);
+        return Quote::quoted()->orderBy('quoted_at',$this->sort )->paginate(5);
     }
     public function render()
     {
--- resources/views/livewire/quote-list.blade.php
+++ resources/views/livewire/quote-list.blade.php
@@ -1,13 +1,13 @@
 <div class="px-3 py-6 lg:px-7">
     <div class="flex items-center justify-between border-b border-gray-100">
         <div class="flex items-center space-x-4 font-light ">
-            <button class="py-4 text-gray-500">Latest</button>
-            <button class="py-4 text-gray-900 border-b border-gray-700">Oldest</button>
+            <button class="py-4 {{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }}" wire:click="setSort('desc')">Latest</button>
+            <button class="py-4 {{ $sort === 'asc'  ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }}" wire:click="setSort('asc')">Oldest</button>
         </div>
     </div>
     <div class="py-4">
         @foreach ($this->quotes as $quote )
-            <x-quotes.quote-item :quote="$quote"/>
+        <x-quotes.quote-item :quote="$quote" />
         @endforeach
 
     </div>
