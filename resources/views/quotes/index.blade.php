8b11e4295a06a9b413e603509896cd96bf7a4d9c
JoshCyril <joshua.cyril88@gmail.com>
2023-12-12 13:51:22 +0000

    🔄️Updated: Alpine Js for search

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -4,7 +4,7 @@
                 @livewire('quote-list')
             </div>
             <div id="side-bar" class="sticky top-0 h-screen col-span-4 px-3 py-6 pt-10 space-y-10 border-t border-gray-100 border-t-gray-100 md:border-t-none md:col-span-1 md:px-6 md:border-l">
-               @livewire('search-box')
+               @include('quotes.partials.search-box')
 
                 <div id="recommended-topics-box">
                     <h3 class="mb-3 text-lg font-semibold text-gray-900">Recommended Topics</h3>
