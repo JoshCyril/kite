63788e9a9c5f9376bfea2de4d81ed63de85a3723
JoshCyril <joshua.cyril88@gmail.com>
2023-12-12 13:31:45 +0000

    ✨Added: Search to sidebar

--- app/Livewire/QuoteList.php
+++ app/Livewire/QuoteList.php
@@ -7,6 +7,9 @@ use Livewire\Attributes\Computed;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Livewire\Attributes\On;
+
+use function Laravel\Prompts\search;
 
 class QuoteList extends Component
 {
@@ -15,14 +18,26 @@ class QuoteList extends Component
     #[Url()]
     public $sort = 'desc';
 
+    #[Url()]
+    public $search = '';
+
     public function setSort($sort) {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
         $this->resetPage();
     }
 
+    #[On('search')]
+    public function updateSearch($search){
+        $this->search = $search;
+        // dd('search');
+    }
+
     #[Computed()]
     public function quotes(){
-        return Quote::quoted()->orderBy('quoted_at',$this->sort )->paginate(5);
+        return Quote::quoted()
+            ->orderBy('quoted_at',$this->sort )
+            ->where('content','like',"%{$this->search}%")
+            ->paginate(5);
     }
     public function render()
     {
--- resources/views/livewire/quote-list.blade.php
+++ resources/views/livewire/quote-list.blade.php
@@ -1,8 +1,13 @@
 <div class="px-3 py-6 lg:px-7">
     <div class="flex items-center justify-between border-b border-gray-100">
+        <div class="text-gray-600">
+            @if($search)
+                Searching {{ $search }}
+            @endif
+        </div>
         <div class="flex items-center space-x-4 font-light ">
-            <button class="py-4 {{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }}" wire:click="setSort('desc')">Latest</button>
-            <button class="py-4 {{ $sort === 'asc'  ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }}" wire:click="setSort('asc')">Oldest</button>
+            <button wire:click="setSort('desc')" class="py-4 {{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }}" >Latest</button>
+            <button wire:click="setSort('asc')"  class="py-4 {{ $sort === 'asc'  ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }}" >Oldest</button>
         </div>
     </div>
     <div class="py-4">
