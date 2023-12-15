86baad9137c748dff74f7136d11a3364b7b6e669
JoshCyril <joshua.cyril88@gmail.com>
2023-12-15 03:43:52 +0000

    Ui fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
             <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
               <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                 <span class="relative inline-block">
-                  <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                  <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-purple-300 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
                     <defs>
                       <pattern id="e77df901-b9d7-4b9b-822e-16b2d410795b" x="0" y="0" width=".135" height=".30">
                         <circle cx="1" cy="1" r=".7"></circle>
@@ -58,7 +58,7 @@
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl font-bold text-red-600">⭕ Latest:</h2>
+        <h2 class="mt-16 mb-5 text-3xl font-bold text-red-600">⭕ Feed:</h2>
             <div class="max-w-full p-0 ">
                 <div class="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
                 @foreach ($latestQuotes as $quote)
